f68d12128b81f40088653491df1dd50646203ebc
zakmoCA <zakeriyaamohamed@gmail.com>
2024-03-08 22:41:25 +1000

    terminal actions

--- main.c
+++ main.c
@@ -1,7 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define EMPTY_CELL " "
+#define FILLED_CELL "█"
+
+int gridWidth, gridHeight;
+
+typedef enum
+{
+  CellAlive,
+  CellDead
+} CellState;
+CellState **currentGrid, **nextGrid;
+
 int main()
 {
   return 0;
+}
+
+void ClearTerminalScreen()
+{
+  printf("\033[2J");
+}
+
+void HideTerminalCursor()
+{
+  printf("\033[?25l");
+}
+
+void ShowTerminalCursor()
+{
+  printf("\033[?25h");
+}
+
+void ResetCursorPosition()
+{
+  printf("\033[H");
 }
